7a1ce97669940b6962f6942d0aebc45d94897740
Johan Bleuzen <johan@bleuzen.fr>
2013-11-07 14:42:49 +0100

    Dirty commit but converter is working all SVG files

--- SVGPathConverter.js
+++ SVGPathConverter.js
@@ -30,69 +30,129 @@ fs.stat(path, function(err, stats) {
   }
 });
 
-function processSvgFile(element, index, array) {
+
+function browseArrayItem(item) {
+  //console.log(item);
+  var obj = item, counter, newPath;
+  for (var i in item) {
+    if (typeof(item[i]) === "object") {
+      //going on step down in the object tree!!
+      if(i === "g") {
+        if (item[i] instanceof Array) {
+          var newObj = [];
+          for(counter in item[i]) {
+            newObj.push(browseArrayItem(item[i][counter]));
+          }
+          obj.g = newObj;
+        } else {
+          obj.g = browseArrayItem(item[i]);
+        }
+
+      }
+
+      if(i === "line") {
+        if(obj.path === undefined) {
+          obj.path = [];
+        }
+        if (item[i] instanceof Array) {
+          for(counter in item[i]) {
+            newPath = transformLineToPath(item[i][counter]);
+            obj.path.push(newPath);
+          }
+        } else {
+          newPath = transformLineToPath(item[i]);
+          obj.path.push(newPath);
+        }
+        delete obj.line;
+      }
+
+      if(i === "ellipse") {
+        if(obj.path === undefined) {
+          obj.path = [];
+        }
+        if (item[i] instanceof Array) {
+          for(counter in item[i]) {
+            newPath = transformEllipseToPath(item[i][counter]);
+            obj.path.push(newPath);
+          }
+        } else {
+          newPath = transformEllipseToPath(item[i]);
+          obj.path.push(newPath);
+        }
+        delete obj.ellipse;
+      }
+
+      if(i === "circle") {
+        if(obj.path === undefined) {
+          obj.path = [];
+        }
+        if (item[i] instanceof Array) {
+          for(counter in item[i]) {
+            newPath = transformCircleToPath(item[i][counter]);
+            obj.path.push(newPath);
+          }
+        } else {
+          newPath = transformCircleToPath(item[i]);
+          obj.path.push(newPath);
+        }
+        delete obj.circle;
+      }
+
+      if(i === "polyline") {
+        if(obj.path === undefined) {
+          obj.path = [];
+        }
+        if (item[i] instanceof Array) {
+          for(counter in item[i]) {
+            newPath =  transformPolylineToPath(item[i][counter]);
+            obj.path.push(newPath);
+          }
+        } else {
+          newPath = transformPolylineToPath(item[i]);
+          obj.path.push(newPath);
+        }
+        delete obj.polyline;
+      }
+
+      if(i === "rect") {
+        // TODO
+        //if(obj.path === undefined) {
+          //obj.path = [];
+        //}
+        //if(obj.path instanceof Object) {
+          //current = obj.path;
+          //obj.path = [];
+          //obj.path.push(current);
+        //}
+        //if (item[i] instanceof Array) {
+          //for(counter in item[i]) {
+            //newPath =  transformRectToPath(item[i][counter]);
+            //obj.path.push(newPath);
+          //}
+        //} else {
+          //newPath = transformRectToPath(item[i]);
+          //obj.path.push(newPath);
+        //}
+        //delete obj.rect;
+      }
+
+    }
+  }
+  return obj;
+}
+
+function processSvgFile(element) {
   fs.readFile(element, 'utf-8', function(err, data) {
-    // TODO : What should we do ?
-    //if(err) {
-    //console.error(err[0]);
-    //return;
-    //}
 
     parsedSvgFile = xm.tojson(data);
     if(parsedSvgFile.svg === undefined)
       return;
 
-    var lines = parsedSvgFile.svg.line;
-    var polylines = parsedSvgFile.svg.polyline;
-    var ellipses = parsedSvgFile.svg.ellipse;
-    var circles = parsedSvgFile.svg.circle;
-
-    // Processing lines
-    if(lines) {
-      if(lines.constructor.name === "Array") {
-        lines.forEach(transformLineToPath);
-      } else {
-        transformLineToPath(lines);
-      }
-
-      delete parsedSvgFile.svg.line;
-    }
-
-    // Processing polylines
-    if(polylines) {
-      if(polylines.constructor.name === "Array") {
-        polylines.forEach(transformPolylineToPath);
-      } else {
-        transformPolylineToPath(polylines);
-      }
-
-      delete parsedSvgFile.svg.polyline;
-    }
-
-    // Processing ellipse
-    if(ellipses) {
-      if(ellipses.constructor.name === "Array") {
-        ellipses.forEach(transformEllipseToPath);
-      } else {
-        transformEllipseToPath(ellipses);
-      }
-
-      delete parsedSvgFile.svg.ellipse;
-    }
-
-    // Processin circle
-    if(circles) {
-      if(circles.constructor.name === "Array") {
-        polylines.forEach(transformCircleToPath);
-      } else {
-        transformCircleToPath(circles);
-      }
-
-      delete parsedSvgFile.svg.circle;
-    }
+    var result = browseArrayItem(parsedSvgFile.svg);
 
     // Fix a bug where we must force root tag to be a <svg>
-    var tmp = { svg : parsedSvgFile.svg};
+    var tmp = { svg :result};
+    //console.log(util.inspect(tmp, false, null));
     var content = '<?xml version="1.0" encoding="utf-8"?>';
     content += xm.toxml(tmp);
 
@@ -110,70 +170,81 @@ function processSvgFile(element, index, array) {
 
 }
 
-function transformLineToPath(element, index, array) {
-  // Adds d path
-  element.class = 'modified-line';
-  element.d = 'M' + element.x1 + ',' + element.y1 + 'L' + element.x2 + ',' + element.y2;
+function transformLineToPath(element) {
+  var newElement = {};
+  newElement.debug = 'modified-line';
+  newElement.d = 'M' + element.x1 + ',' + element.y1 + 'L' + element.x2 + ',' + element.y2;
+  if(element.stroke)
+    newElement.stroke = element.stroke;
+  if(element.fill)
+    newElement.fill = element.fill;
 
-  // Remove useless attributes
-  delete element.x1;
-  delete element.y1;
-  delete element.x2;
-  delete element.y2;
-
-  parsedSvgFile.svg.path.push(element);
+  return newElement;
 }
 
-function transformPolylineToPath(element, index, array) {
-  element.class = 'modified-polyline';
+function transformRectToPath(element) {
+  var newElement = {};
+  newElement.debug = 'modified-rect';
+  return newElement;
+}
+
+function transformPolylineToPath(element) {
+  var newElement = {};
+  newElement.debug = 'modified-polyline';
   var points = element.points.split(' ');
   var path = "M" + points[0];
   for(var i = 1; i < points.length; i++) {
     path += "L"+points[i];
   }
-  element.d = path;
+  newElement.d = path;
 
-  // Remove useless attributes
-  delete element.points;
+  if(element.stroke)
+    newElement.stroke = element.stroke;
+  if(element.fill)
+    newElement.fill = element.fill;
 
-  parsedSvgFile.svg.path.push(element);
+  return newElement;
 }
 
-function transformEllipseToPath(element, index, array) {
+function transformEllipseToPath(element) {
   var startX = element.cx - element.rx,
       startY = element.cy;
   var endX = parseFloat(element.cx) + parseFloat(element.rx),
       endY = element.cy;
-  element.class = 'modified-ellipse';
-  element.d = "M" + startX + "," + startY +
-              "A" + element.rx + "," + element.ry + " 0,1,1 " + endX + "," + endY +
-              "A" + element.rx + "," + element.ry + " 0,1,1 " + startX + "," + endY;
 
-  // Remove useless attributes
-  delete element.cx;
-  delete element.cy;
-  delete element.rx;
-  delete element.ry;
+  var newElement = {};
+  newElement.debug = 'modified-ellipse';
+  newElement.d = "M" + startX + "," + startY +
+                 "A" + element.rx + "," + element.ry + " 0,1,1 " + endX + "," + endY +
+                 "A" + element.rx + "," + element.ry + " 0,1,1 " + startX + "," + endY;
 
-  parsedSvgFile.svg.path.push(element);
+  if(element.stroke)
+    newElement.stroke = element.stroke;
+  if(element.fill)
+    newElement.fill = element.fill;
+  if(element.transform)
+    newElement.transform = element.transform;
+
+  return newElement;
 }
 
 function transformCircleToPath(element, index, array) {
+  var newElement = {};
   var startX = element.cx - element.r,
       startY = element.cy;
   var endX = parseFloat(element.cx) + parseFloat(element.r),
       endY = element.cy;
-  element.class = 'modified-ellipse';
-  element.d = "M" + startX + "," + startY +
+  newElement.debug = 'modified-ellipse';
+  newElement.d = "M" + startX + "," + startY +
               "A" + element.r + "," + element.r + " 0,1,1 " + endX + "," + endY +
               "A" + element.r + "," + element.r + " 0,1,1 " + startX + "," + endY;
 
-  // Remove useless attributes
-  delete element.cx;
-  delete element.cy;
-  delete element.r;
+  if(element.stroke)
+    newElement.stroke = element.stroke;
+  if(element.fill)
+    newElement.fill = element.fill;
 
-  parsedSvgFile.svg.path.push(element);
+  return newElement;
 }
 
 
